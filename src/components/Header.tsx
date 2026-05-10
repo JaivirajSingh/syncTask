348006e488f7a612806b9afee4ad5c9bcf060b99
JS <jjsingh1201@gmail.com>
2026-05-10 20:22:23 +0530

    add sign out functionality

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,38 @@
-
+import { useAuth } from "../context/AuthContext"
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
 
 export default function Header() {
+    const { signOutUser, session } = useAuth()
+    const navigate = useNavigate()
+    const [error, setError] = useState(null)
 
+    const handleSignOut = async (e) => {
+        e.preventDefault()
+
+        const { success, error } = await signOutUser()
+        if (success) {
+            navigate("/")
+        } else {
+            setError(error.message)
+        }
+    }
 
     return (
         <>
-            <button>
+            <button onClick={handleSignOut}>
                 Sign Out
             </button>
+
+            <h2>
+                {session?.user?.email}
+            </h2>
+
+            {error && (
+                <div role="role">
+                    {error}
+                </div>
+            )}
         </>
     )
 }
