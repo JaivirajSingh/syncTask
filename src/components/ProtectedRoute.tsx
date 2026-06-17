577f9bb16e2c2e3e52ebb3946b902fcae37c529b
JS <jjsingh1201@gmail.com>
2026-06-17 15:12:34 +0530

    Fix children error

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,12 +1,10 @@
 import { Navigate } from "react-router-dom"
 import { useAuth } from "../context/AuthContext"
 
-const ProtectedRoute = () => {
+const ProtectedRoute = ({ children }: { children?: React.ReactNode}) => {
     const { session } = useAuth()
 
-    if (session === undefined) {
-        return <div>Loading...</div>
-    }
+    if (session === undefined) return <div>Loading...</div>
 
     // Doesn't let user go to dashboard without signing in
     return session ? <>{children}</> : <Navigate to="/SignIn"/>
